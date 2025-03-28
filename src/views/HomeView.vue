4b5813f0ac6ae32a77f3437858133abac79b0faa
Swino4ka <83330628+Swino4ka@users.noreply.github.com>
2025-03-28 09:55:05 +0100

    Base

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <BaseLayout>
+    <ApiInteraction />
+  </BaseLayout>
+</template>
+
+<script>
+import ApiInteraction from '@/components/ApiInteraction.vue';
+import BaseLayout from '@/layouts/BaseLayout.vue';
+
+export default {
+  components: {
+    ApiInteraction,
+    BaseLayout
+  }
+};
+</script>
