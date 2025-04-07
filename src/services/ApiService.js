0e5ae4ae4c7c6003625d89e4895a7cc03ec8fbcb
Swino4ka <83330628+Swino4ka@users.noreply.github.com>
2025-04-07 08:00:24 +0200

    Update ApiService.js

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://api.com', // Will change later
+  baseURL: 'https://api.com',
   headers: {
     'Content-Type': 'application/json',
   },
